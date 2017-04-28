680136147c704d2295c678763f545da3bc544931
Byron Torres <btorres@thoughtworks.com>
2017-04-28 01:58:37 -0500

    LC-1: Byron - Add Greeting example controller.

---
+++ src/test/java/byleto/listascontrol/controller/GreetingControllerTest.java
@@ -0,0 +1,14 @@
+package byleto.listascontrol.controller;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class GreetingControllerTest {
+
+    @Test
+    public void should() throws Exception {
+        assertThat("a", is("a"));
+    }
+}
